da1d434708393c0a37c81a780a48d261952c7c70
MBPR <wabesasa@gmail.com>
2016-03-15 16:11:56 +0800

    V0.1.0(uncompleted)
    just add import image and detect edge by Canny by OpenCV

--- Cirecle-Detection/main.cpp
+++ Cirecle-Detection/main.cpp
@@ -7,9 +7,73 @@
 //
 
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace::std;
+using namespace::cv;
+
+Mat src_img, src_gray;
+Mat dst, detected_edges;
+int width, height;
+int ratio = 3;
+int kernel_size = 3;
+int lowThreshold;
+String window_name = "Result window";
+
+void CannyThreshold(int, void*) {
+    blur(src_gray, detected_edges, Size(3,3));
+    
+    Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*3,  kernel_size);
+    
+    dst = Scalar::all(0);
+    
+    src_img.copyTo(dst, detected_edges);
+    imshow(window_name, dst);
+}
+
+void printMap(Mat img) {
+    width = img.size().width;
+    height = img.size().height;
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            Scalar intensity = img.at<uchar>(j, i);
+            //cout << "(" << i << ", " << j << ")";
+            cout << intensity[0] << " ";
+            if (j == height - 1) {
+                cout << endl;
+            }
+        }
+    }
+}
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    src_img = imread("circle.bmp");
+    
+    if (src_img.empty()) {
+        cout << "Could not open or find the image" << endl;
+        return -1;
+    }
+    
+    dst.create(src_img.size(), src_img.type());
+    cvtColor(src_img, src_gray, CV_BGR2GRAY);
+    
+    CannyThreshold(0, 0);
+    
+    // print pixels
+    //printMap(dst);
+    
+    
+    namedWindow(window_name, WINDOW_AUTOSIZE);
+    //imshow(window_name, src_img);
+    
+    waitKey(0);
+    
     return 0;
 }
+
