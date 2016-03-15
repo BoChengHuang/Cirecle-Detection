7723ad2be2119e31ee72239fb48430fbbcf2a365
MBPR <wabesasa@gmail.com>
2016-03-15 15:05:56 +0800

    Initial Commit

---
+++ Cirecle-Detection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cirecle-Detection
+//
+//  Created by Ives on 3/15/16.
+//  Copyright © 2016 bochengw.twbbs.org. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
